52b8b6b572550b5a01ac8eac4b33e4a04c15e701
BeaGabriela <biiagabrielaa@gmail.com>
2023-08-16 14:50:25 -0300

    Alterado de acordo com o id topico as comidas

--- PROJETOS/cafeteria/frontEnd/HOME/index.js
+++ PROJETOS/cafeteria/frontEnd/HOME/index.js
@@ -65,7 +65,7 @@ function carregar() {
                 }
 
                 u.forEach(comida => {
-                    if(comida.id_topico == 3){
+                    if(comida.id_topico == 1){
                     const DivQuadrado = modeloQuadrado.cloneNode(true);
                     DivQuadrado.classList.remove('model');
                     DivQuadrado.querySelector('#cappucino').src = '../imgs/' + comida.img;
@@ -79,11 +79,56 @@ function carregar() {
                     DivQuadrado.appendChild(imgBotao);
 
                     alinhandoQuadrado.appendChild(DivQuadrado);
-                    }
-                });
-            });
-    }
+                    }else if(comida.id_topico == 2){
+                        const QuadradoCha = document.querySelector('.quadradoCha').cloneNode(true)
+                        QuadradoCha.classList.remove('model')
+                        QuadradoCha.querySelector('#chaI').src =  '../imgs/' + comida.img;
+                        QuadradoCha.querySelector('#nomecha').innerHTML = comida.nome;
+                        QuadradoCha.querySelector('#valorCham').innerHTML = comida.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    
+                        const imgBotao = document.createElement('img');
+                        imgBotao.src = '../imgs/+.png';
+                        imgBotao.height = '20vh';
+                        imgBotao.onclick = () => VisualizarItem(comida.id);
+                        QuadradoCha.appendChild(imgBotao);
+    
+                        document.querySelector('.Alinhandochas').appendChild(QuadradoCha);
+                        
 
+                    }else if(comida.id_topico == 3){
+                        const QuadradoChoc = document.querySelector('.quadradoChocolate').cloneNode(true)
+                        QuadradoChoc.classList.remove('model')
+                        QuadradoChoc.querySelector('#chocolate').src =  '../imgs/' + comida.img;
+                        QuadradoChoc.querySelector('#nomechocolate').innerHTML = comida.nome;
+                        QuadradoChoc.querySelector('#valorChoco').innerHTML = comida.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    
+                        const imgBotao = document.createElement('img');
+                        imgBotao.src = '../imgs/+.png';
+                        imgBotao.height = '20vh';
+                        imgBotao.onclick = () => VisualizarItem(comida.id);
+                        QuadradoChoc.appendChild(imgBotao);
+    
+                        document.querySelector('.AlinhandoChoco').appendChild(QuadradoChoc)
+                    }else if(comida.id_topico == 4){
+                        const quadradoomidaGeral = document.querySelector('.quadradoomidaGeral').cloneNode(true)
+                        quadradoomidaGeral.classList.remove('model')
+                        quadradoomidaGeral.querySelector('#comidas').src =  '../imgs/' + comida.img;
+                        quadradoomidaGeral.querySelector('#comidasGeral').innerHTML = comida.nome;
+                        quadradoomidaGeral.querySelector('#valorC').innerHTML = comida.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    
+                        const imgBotao = document.createElement('img');
+                        imgBotao.src = '../imgs/+.png';
+                        imgBotao.height = '20vh';
+                        imgBotao.onclick = () => VisualizarItem(comida.id);
+                        quadradoomidaGeral.appendChild(imgBotao);
+    
+                        document.querySelector('.alinhandoComida').appendChild(quadradoomidaGeral)
+                    }
+                      
+                    })
+                })
+            }
+                    
     procurar.addEventListener('input', () => {
         procurando = procurar.value;
         procurando = procurando.charAt(0).toUpperCase() + procurando.slice(1); // Capitaliza a primeira letra
@@ -204,4 +249,3 @@ function VisualizarItemComidas(id) {
 //         return `../assets/cha.png`
 //     }
 // }
-
