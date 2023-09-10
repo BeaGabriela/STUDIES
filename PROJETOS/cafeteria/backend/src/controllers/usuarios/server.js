a8d93ecb917d2c77ef9b765ce45adaa87b19e766
BeaGabriela <biiagabrielaa@gmail.com>
2023-09-10 14:37:11 -0300

    Criando rota de confirmação de email

---
+++ PROJETOS/cafeteria/backend/src/controllers/usuarios/server.js
@@ -0,0 +1,54 @@
+// server.js
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const nodemailer = require('nodemailer');
+
+const prisma = new PrismaClient();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Rota para enviar um email de confirmação
+app.post('/enviar-email', async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email,
+      },
+    });
+
+    // Configurar o transporte do nodemailer
+    const transporter = nodemailer.createTransport({
+      // Configurações de transporte de email (Gmail, SMTP, etc.)
+    });
+
+    // Configurar o email de confirmação
+    const mailOptions = {
+      from: 'seu-email@gmail.com', // Seu email
+      to: email, // Email do destinatário
+      subject: 'Confirme seu email',
+      text: `Clique no link a seguir para confirmar seu email: http://seusite.com/confirmar/${user.id}`,
+    };
+
+    // Enviar o email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Erro ao enviar o email de confirmação');
+      } else {
+        console.log('Email de confirmação enviado: ' + info.response);
+        res.status(200).send('Email de confirmação enviado');
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao criar o usuário');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
