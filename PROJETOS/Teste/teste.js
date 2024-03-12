71b5713e5ed109548fa4866b8cbc04ac9430233e
BeaGabriela <biiagabrielaa@gmail.com>
2024-03-12 14:27:19 -0300

    Criação do onclick e da animação da "voar" pela tela. Alem da edição do fundo que será alterada logo

---
+++ PROJETOS/Teste/teste.js
@@ -0,0 +1,18 @@
+
+function carregar(){
+
+}
+
+
+
+
+var bruxa = document.querySelector('.escolher')
+
+function clicandoSim(){
+    bruxa.style.top = '43vh'
+    bruxa.style.animation ='voar 2s ease forwards'
+}
+
+function clicandoNao(){
+    bruxa.style.top= '55vh'
+}
