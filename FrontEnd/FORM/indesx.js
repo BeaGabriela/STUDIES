b04df0da7e07a25e5ea63bb5079704e314c5bb68
BeaGabriela <biiagabrielaa@gmail.com>
2024-07-02 13:33:53 -0300

    criação do modal e das funções necessarias

--- FrontEnd/FORM/indesx.js
+++ FrontEnd/FORM/indesx.js
@@ -1,28 +1,45 @@
 var PrimeiroNome = document.querySelector('#primeiroNome');
 var UltimoNome = document.querySelector('#ultimoNome');
+var Email = document.querySelector('#email');
 
-
-
-function carregar() {
-
-}
 
 
 
 
 const enviar = () => {
+    // Validando o primeiro nome
     if (PrimeiroNome.value == 0 || PrimeiroNome.value == ' ') {
-        console.log('erro 1')
-       
+        document.querySelector('.requiredNome').classList.remove('model')
+        PrimeiroNome.style.borderColor = 'red';
     } else {
         console.log('ok 1')
     }
 
+    // Validando o ultimo nome
     if (UltimoNome.value == 0 || UltimoNome.value == ' ') {
-        console.log('erro 2');
+        document.querySelector('.requiredNomeUltimo').classList.remove('model')
+        UltimoNome.style.borderColor = 'red';
     } else {
         console.log('ok 2')
     }
 
+    // Validando o Email
+    if(Email.value == 0 || Email.value == " " || validarEmail(false) ){
+        document.querySelector('.requiredEmail').classList.remove('model')
+        Email.style.borderColor ='red';
+    }else{
+        console.log('ok')
+    }
+}
 
+function validarEmail() {
+    const email = document.getElementById('email').value;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    if (regex.test(email)) {
+        return true; // Continua com o envio do formulário
+    } else {
+        alert('Email inválido! Por favor, insira um email válido.');
+        return false; // Impede o envio do formulário
+    }
 }
