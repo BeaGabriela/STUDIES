23a236b47bd30267aa1451de290d4555c1666501
BeaGabriela <biiagabrielaa@gmail.com>
2024-07-11 13:27:58 -0300

    Adequando o input checkbox ao uso, mudando o funcinamento

--- FrontEnd/FORM/indesx.js
+++ FrontEnd/FORM/indesx.js
@@ -24,22 +24,74 @@ const enviar = () => {
     }
 
     // Validando o Email
-    if(Email.value == 0 || Email.value == " " || validarEmail(false) ){
+    if (Email.value == 0 || Email.value == " " || validarEmail(false)) {
         document.querySelector('.requiredEmail').classList.remove('model')
-        Email.style.borderColor ='red';
-    }else{
+        Email.style.borderColor = 'red';
+    } else {
         console.log('ok')
     }
+
+    validandoInput()
 }
 
+
+// Função criada para validar email
 function validarEmail() {
     const email = document.getElementById('email').value;
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
+
     if (regex.test(email)) {
         return true; // Continua com o envio do formulário
     } else {
         alert('Email inválido! Por favor, insira um email válido.');
         return false; // Impede o envio do formulário
     }
+
+}
+
+
+//Funçãp criada pra averiguar se os inputs do tipo checkbox foram clicados.
+function validandoInput() {
+    var erro = document.querySelector('.requiredInput')
+    var inputGeneral = document.querySelector('#inputG');
+    var inputSupport = document.querySelector('#inputS');
+
+
+    // Seleciona todos os checkboxes do formulário
+    var checkboxes = document.querySelectorAll('input[type="radio"]');
+
+    // Variáveis para armazenar o status de cada tipo de checkbox
+    var isGeneralChecked = false;
+    var isSupportChecked = false;
+
+    // Verifica cada checkbox
+    checkboxes.forEach(function (checkbox) {
+        if (checkbox.checked) {
+            if (checkbox.value === 'general') {
+                isGeneralChecked = true;
+            } else if (checkbox.value === 'support') {
+                isSupportChecked = true;
+            }
+        }
+    });
+
+    // Verifica qual tipo de checkbox está marcado e exibe a mensagem correspondente
+    if (isGeneralChecked && isSupportChecked) {
+        alert('Erro!')
+    } else if (isGeneralChecked) {
+        console.log('Apenas o checkbox General está marcado');
+        erro.classList.add('model');
+        inputGeneral.style.borderColor = '#000';
+        inputSupport.style.borderColor = '#000';
+    } else if (isSupportChecked) {
+        console.log('Apenas o checkbox Support está marcado');
+        erro.classList.add('model');
+        inputGeneral.style.borderColor = '#000';
+        inputSupport.style.borderColor = '#000';
+    } else {
+        erro.classList.remove('model');
+        inputGeneral.style.borderColor = 'red';
+        inputSupport.style.borderColor = 'red';
+    }
+
 }
