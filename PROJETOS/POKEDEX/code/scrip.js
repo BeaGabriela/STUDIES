2217fb14370128be234029d01bfdbd7d3b8ff5f5
BeaGabriela <biiagabrielaa@gmail.com>
2024-07-24 14:26:29 -0300

    Adicionando as funções de script, mosntrando a img do pokemon e o nome

---
+++ PROJETOS/POKEDEX/code/scrip.js
@@ -0,0 +1,58 @@
+// Criando uma variavel para chamar a imagem dopokemon do html
+const imagemPokemon = document.querySelector('#pokemonImagem')
+
+// Criando uma variavel para chamar o nome do pokemon do html
+const nomePokemon = document.querySelector('#nome')
+
+// Criando uma variavel para chamar o numero do pokemon do html
+const numeroPokemon = document.querySelector('#numeroPokemon')
+
+// Criando uma variavel com o valor definindo como 1
+let NumeroIdpokemon = 1;
+
+
+// Criar uma funcão assicrona para chamar a API do pokemon
+const fetchPokemon = async (pokemon) => {
+    // Criar uma constante com o valor definido como a api
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    console.log(pokemon)
+    // status 200 = 'tudo ok'
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json()
+        return data
+    }
+}
+
+// Criar uma função ára direcionar as caracteristicas do pokemon
+const direcionandoPokemon = async (PokemonId) => {
+    // Criando um nome para quando não houver  o pokemon
+    nomePokemon.innerHTML = 'Carregando...';
+    numeroPokemon.innerHTML = '';
+
+    // Chamando a APIResponse
+    const data = await fetchPokemon(PokemonId)
+
+    if (data) {
+        // Direcionando a imagem do pokemon para usar display block
+        imagemPokemon.style.display = 'block';
+        //Definindo o nome do pokemon baseado no json
+        nomePokemon.innerHTML = data.name;
+        //Definindo o id do pokemon
+        numeroPokemon.innerHTML = data.id;
+        // Defininfo que avariavel declarada na parte superior. agora tem o valor do id
+        NumeroIdpokemon = data.id
+
+
+        // Definindo a imagem do pokemon
+        imagemPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+
+    } else {
+        nomePokemon.innerHTML = 'Não encontrado';
+        numeroPokemon.innerHTML = '';
+        imagemPokemon.style.display = 'none';
+    }
+
+
+}
+
+direcionandoPokemon(NumeroIdpokemon)
