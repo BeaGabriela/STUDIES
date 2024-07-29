c7e70bc8eb2ffc3b9080be8e083a76be89210a85
BeaGabriela <biiagabrielaa@gmail.com>
2024-07-29 10:29:41 -0300

    Aderindo as hablidades de acordo com cada pokemon

--- PROJETOS/POKEDEX/code/scrip.js
+++ PROJETOS/POKEDEX/code/scrip.js
@@ -1,4 +1,4 @@
-// Criando uma variavel para chamar a imagem dopokemon do html
+//  Criando uma variavel para chamar a imagem dopokemon do html
 const imagemPokemon = document.querySelector('#pokemonImagem');
 
 // Criando uma variavel para chamar o nome do pokemon do html
@@ -13,8 +13,8 @@ const btnSetaAvanco = document.querySelector('.setaAvanco');
 // Criando um botão para chamar a função para buscar o pokemon anterior
 const btnSetaRecuo = document.querySelector('.setaRecuo');
 
-//Chamando o input para qye seja ossivel resgatar seuy valor e utilizar para uma busca na lista de pokemons
-const inputPesquisa = document.querySelector('#inputPesquisaPokemon');
+// Criando o campo de habilidades dos pokemons
+const habilidades = document.querySelector('#habilidades');
 
 //Chamndo o botão que dará gatilho para iniciar a busca na lista
 const btnBusca = document.querySelector('#buscarPokemon');
@@ -40,6 +40,7 @@ const direcionandoPokemon = async (PokemonId) => {
     // Criando um nome para quando não houver  o pokemon
     nomePokemon.innerHTML = 'Carregando...';
     numeroPokemon.innerHTML = '';
+    habilidades.innerHTML = '';
 
     // Chamando a APIResponse
     const data = await fetchPokemon(PokemonId)
@@ -58,6 +59,16 @@ const direcionandoPokemon = async (PokemonId) => {
         // Definindo a imagem do pokemon
         imagemPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
 
+        // definindo as habilidades dos pokemons
+        if (data.abilities.length > 0) {
+            data.abilities.forEach((item) => {
+                const habilidadeItem = document.createElement('p');
+                habilidadeItem.textContent = `Habilidade: ${item.ability.name}`;
+                habilidades.appendChild(habilidadeItem);
+            });
+        } else {
+            habilidades.innerHTML = 'Nenhuma habilidade encontrada';
+        }
     } else {
         nomePokemon.innerHTML = 'Não encontrado';
         numeroPokemon.innerHTML = '';
@@ -66,13 +77,6 @@ const direcionandoPokemon = async (PokemonId) => {
 
 
 }
-
-//Criando uma função que busca o pokemon de acordo com o nome colocado no input
-btnBusca.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    direcionandoPokemon(inputPesquisa.value.toLowerCase())
-})
 
 
 //Criando a função de acrescentar um numero no id do pokemon e ir para o proximo
